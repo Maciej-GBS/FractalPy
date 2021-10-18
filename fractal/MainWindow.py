395624ec8c440989c4580f1fbef8d5c51eec2492
Maciej-GBS <12584491+Maciej-GBS@users.noreply.github.com>
2021-10-18 15:01:20 +0200

    Migrated missing information

--- fractal/MainWindow.py
+++ fractal/MainWindow.py
@@ -6,6 +6,7 @@ class MainWindow(QtWidgets.QMainWindow):
     def __init__(self):
         super(MainWindow, self).__init__()
         self.layout_object = MainWindowLayout(self)
+        self.layout_object.setupUi()
 
 class MainWindowLayout(object):
     def __init__(self, owner:MainWindow):
@@ -16,110 +17,145 @@ class MainWindowLayout(object):
         self.owner.setWindowTitle("Fractal Generator")
         self.owner.setObjectName("MainWindow")
         self.owner.resize(944, 697)
+
         self.centralwidget = QtWidgets.QWidget(self.owner)
         self.centralwidget.setObjectName("centralwidget")
+
         self.gridLayout_2 = QtWidgets.QGridLayout(self.centralwidget)
         self.gridLayout_2.setObjectName("gridLayout_2")
+
         self.gridLayout = QtWidgets.QGridLayout()
         self.gridLayout.setSizeConstraint(QtWidgets.QLayout.SetDefaultConstraint)
         self.gridLayout.setObjectName("gridLayout")
+
         self.gText = QtWidgets.QPlainTextEdit(self.centralwidget)
         self.gText.setObjectName("gText")
+        self.gText.setPlainText("1")
         self.gridLayout.addWidget(self.gText, 14, 0, 1, 1)
+
         self.label_4 = QtWidgets.QLabel(self.centralwidget)
         self.label_4.setObjectName("label_4")
+        self.label_4.setText("Constant FI:")
         self.gridLayout.addWidget(self.label_4, 9, 0, 1, 1)
+
         self.zoomSpin = QtWidgets.QDoubleSpinBox(self.centralwidget)
         self.zoomSpin.setDecimals(4)
         self.zoomSpin.setMinimum(0.0001)
         self.zoomSpin.setMaximum(100000.0)
         self.zoomSpin.setSingleStep(0.1)
-        self.zoomSpin.setProperty("value", 1.0)
+        self.zoomSpin.setValue(1.0)
         self.zoomSpin.setObjectName("zoomSpin")
         self.gridLayout.addWidget(self.zoomSpin, 7, 0, 1, 1)
+
         self.label = QtWidgets.QLabel(self.centralwidget)
         self.label.setObjectName("label")
+        self.label.setText("Colormap:")
         self.gridLayout.addWidget(self.label, 4, 0, 1, 1)
+
         self.savButton = QtWidgets.QPushButton(self.centralwidget)
         self.savButton.setObjectName("savButton")
+        self.savButton.setText("Save")
         self.gridLayout.addWidget(self.savButton, 0, 0, 1, 1)
+
         self.progressBar = QtWidgets.QProgressBar(self.centralwidget)
-        self.progressBar.setProperty("value", 100)
+        self.progressBar.setValue(100)
         self.progressBar.setObjectName("progressBar")
         self.gridLayout.addWidget(self.progressBar, 16, 0, 1, 1)
+
         self.genButton = QtWidgets.QPushButton(self.centralwidget)
         self.genButton.setObjectName("genButton")
+        self.genButton.setText("Generate")
         self.gridLayout.addWidget(self.genButton, 1, 0, 1, 1)
+
         self.label_3 = QtWidgets.QLabel(self.centralwidget)
         self.label_3.setObjectName("label_3")
+        self.label_3.setText("Zoom:")
         self.gridLayout.addWidget(self.label_3, 6, 0, 1, 1)
+
         self.graphicsView = CustomGraphics(self.centralwidget)
-        self.graphicsView.setProperty("cursor", QtCore.Qt.OpenHandCursor)
+        self.graphicsView.setCursor(QtCore.Qt.OpenHandCursor)
         self.graphicsView.setObjectName("graphicsView")
         self.gridLayout.addWidget(self.graphicsView, 0, 1, 17, 2)
+
         self.label_2 = QtWidgets.QLabel(self.centralwidget)
         self.label_2.setObjectName("label_2")
+        self.label_2.setText("Iterations:")
         self.gridLayout.addWidget(self.label_2, 2, 0, 1, 1)
+
         self.iterSpin = QtWidgets.QSpinBox(self.centralwidget)
         self.iterSpin.setMinimum(1)
         self.iterSpin.setMaximum(1000000)
         self.iterSpin.setSingleStep(10)
-        self.iterSpin.setProperty("value", 100)
+        self.iterSpin.setValue(100)
         self.iterSpin.setObjectName("iterSpin")
         self.gridLayout.addWidget(self.iterSpin, 3, 0, 1, 1)
+
         self.fiSlider = QtWidgets.QSlider(self.centralwidget)
         self.fiSlider.setMaximum(1000)
         self.fiSlider.setOrientation(QtCore.Qt.Horizontal)
         self.fiSlider.setObjectName("fiSlider")
         self.gridLayout.addWidget(self.fiSlider, 10, 0, 1, 1)
+
         self.label_5 = QtWidgets.QLabel(self.centralwidget)
         self.label_5.setObjectName("label_5")
+        self.label_5.setText("Constant R:")
         self.gridLayout.addWidget(self.label_5, 11, 0, 1, 1)
+
         self.rSpin = QtWidgets.QDoubleSpinBox(self.centralwidget)
         self.rSpin.setDecimals(4)
         self.rSpin.setMaximum(100.0)
         self.rSpin.setSingleStep(0.1)
         self.rSpin.setObjectName("rSpin")
         self.gridLayout.addWidget(self.rSpin, 12, 0, 1, 1)
+
         self.infoLabel = QtWidgets.QLabel(self.centralwidget)
         self.infoLabel.setObjectName("infoLabel")
+        self.infoLabel.setText("Click generate")
         self.gridLayout.addWidget(self.infoLabel, 15, 0, 1, 1)
+
         self.fText = QtWidgets.QPlainTextEdit(self.centralwidget)
         self.fText.setMinimumSize(QtCore.QSize(0, 0))
         self.fText.setObjectName("fText")
+        self.fText.setPlainText("1z^2")
         self.gridLayout.addWidget(self.fText, 13, 0, 1, 1)
+
         self.horizontalLayout = QtWidgets.QHBoxLayout()
         self.horizontalLayout.setObjectName("horizontalLayout")
         self.colorSpin = CustomSpinner(self.centralwidget)
         self.colorSpin.setMaximum(16777215)
         self.colorSpin.setSingleStep(1)
-        self.colorSpin.setProperty("value", 16777215)
+        self.colorSpin.setValue(16777215)
         self.colorSpin.setObjectName("colorSpin")
+        self.colorSpin.setPrefix("0x")
         self.horizontalLayout.addWidget(self.colorSpin)
         self.shiftSpin = QtWidgets.QSpinBox(self.centralwidget)
         self.shiftSpin.setMinimum(1)
         self.shiftSpin.setMaximum(6)
-        self.shiftSpin.setProperty("value", 1)
+        self.shiftSpin.setValue(1)
         self.shiftSpin.setObjectName("shiftSpin")
         self.horizontalLayout.addWidget(self.shiftSpin)
         self.gridLayout.addLayout(self.horizontalLayout, 5, 0, 1, 1)
+
         self.resetButton = QtWidgets.QPushButton(self.centralwidget)
         self.resetButton.setObjectName("resetButton")
+        self.resetButton.setText("Reset position")
         self.gridLayout.addWidget(self.resetButton, 8, 0, 1, 1)
+
         self.gridLayout.setColumnStretch(0, 1)
         self.gridLayout.setColumnStretch(1, 6)
         self.gridLayout_2.addLayout(self.gridLayout, 0, 0, 1, 1)
+
         self.owner.setCentralWidget(self.centralwidget)
+
         self.menubar = QtWidgets.QMenuBar(self.owner)
         self.menubar.setGeometry(QtCore.QRect(0, 0, 944, 25))
         self.menubar.setObjectName("menubar")
         self.owner.setMenuBar(self.menubar)
+
         self.statusbar = QtWidgets.QStatusBar(self.owner)
         self.statusbar.setObjectName("statusbar")
         self.owner.setStatusBar(self.statusbar)
 
-        self.owner.retranslateUi(self.owner)
-        QtCore.QMetaObject.connectSlotsByName(self.owner)
         self.owner.setTabOrder(self.savButton, self.genButton)
         self.owner.setTabOrder(self.genButton, self.iterSpin)
         self.owner.setTabOrder(self.iterSpin, self.colorSpin)
