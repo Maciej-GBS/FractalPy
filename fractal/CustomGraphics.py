67087e8b58dccbbca0425d99e09269007ebf9b76
Maciej-GBS <12584491+Maciej-GBS@users.noreply.github.com>
2021-10-18 21:35:10 +0200

    Mouse events added to CustomGraphics

--- fractal/CustomGraphics.py
+++ fractal/CustomGraphics.py
@@ -1,5 +1,17 @@
+from PySide2.QtCore import Qt
 from PySide2.QtWidgets import QGraphicsView
 
 class CustomGraphics(QGraphicsView):
     def __init__(self, scene, parent=None):
         super().__init__(scene, parent)
+
+    def mousePressEvent(self, event):
+        super().mousePressEvent(event)
+        self.setCursor(Qt.ClosedHandCursor)
+        self.mousePos = (event.x(), event.y())
+
+    def mouseReleaseEvent(self, event):
+        super().mouseReleaseEvent(event)
+        self.setCursor(Qt.OpenHandCursor)
+        dPos = [x - x0 for x0,x in zip(self.mousePos, [event.x(), event.y()])]
+        # TODO reposition graphic
