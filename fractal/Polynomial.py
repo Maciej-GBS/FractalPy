4ccf9f04776da50a899da8f61dadadd4a2b1797e
Maciej-GBS <12584491+Maciej-GBS@users.noreply.github.com>
2021-10-21 19:17:06 +0200

    Polynomial structure added

--- fractal/Polynomial.py
+++ fractal/Polynomial.py
@@ -1,3 +1,33 @@
 class Polynomial:
     def __init__(self):
         pass
+
+    def __init__(self, s: str):
+        pass
+
+    def __init__(self, factors: list):
+        pass
+
+    def __init__(self, *factors: complex):
+        pass
+
+    def __call__(self, x: complex):
+        return self.calc(x)
+
+    def __len__(self):
+        pass
+
+    def __getitem__(self, key):
+        pass
+
+    def __setitem__(self, key, value):
+        pass
+
+    def __iter__(self):
+        pass
+
+    def __str__(self):
+        pass
+
+    def calc(self, x: complex):
+        pass
