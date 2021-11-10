2a27d7fb3c2a4b504ec04b4867da18c9996ac8f1
Maciej-GBS <12584491+Maciej-GBS@users.noreply.github.com>
2021-11-10 15:39:50 +0100

    Implemented Polynomial from string

--- fractal/Polynomial.py
+++ fractal/Polynomial.py
@@ -1,4 +1,5 @@
 import numpy as np
+import re
 
 from typing import Union
 
@@ -6,13 +7,39 @@ from typing import Union
 class Polynomial:
     def __init__(self, factors: Union[str, list] = [0]):
         if isinstance(factors, str):
-            # TODO: parsing from string
-            pass
+            self.from_str(factors)
         else:
             self._poly = np.poly1d(factors, variable='x')
 
-    # def __init__(self, *factors: complex):
-    #     pass
+    def from_str(self, factorstr: str):
+        poly = []
+        def add_atom(atom, p):
+            while len(poly) <= p:
+                poly.append(complex(0.0))
+            poly[len(poly) - 1 - p] += atom
+        truncated = factorstr.replace(" ", "")
+        patterns = [
+            "[\-]?[(][0-9.j+\-]*[)]x[0-9^]{2,}",
+            "[\-]?[(][0-9.j+\-]*[)]x",
+            "[\-]?[(][0-9.j+\-]*[)]",
+            "[0-9.j\-]*x[0-9^]{2,}",
+            "[0-9.j\-]*x",
+            "[0-9.j\-]+"
+        ]
+        fix_sign = lambda s: s.replace("-(","(-").replace("--","-")
+        for regex in patterns:
+            matches = re.findall(regex, truncated)
+            for x in matches:
+                truncated = truncated.replace(x, "")
+                if 'x^' in x:
+                    sides = x.split('x^')
+                    add_atom(complex(fix_sign(sides[0])), int(sides[1]))
+                elif 'x' in x:
+                    sides = x.split('x')
+                    add_atom(complex(fix_sign(sides[0])), 1)
+                else:
+                    add_atom(complex(x), 0)
+        self._poly = np.poly1d(poly, variable='x')
 
     def __call__(self, x: complex):
         return self.calc(x)
@@ -27,7 +54,7 @@ class Polynomial:
         self._poly.coeffs[key] = value
 
     def __iter__(self):
-        return self._poly.coeffs.__iter__()
+        return iter(self._poly.coeffs)
 
     def __str__(self):
         atom_list = []
