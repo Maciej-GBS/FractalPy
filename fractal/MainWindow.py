5138ab885e66801205af16f3d1f897ce9bdb82f7
Maciej-GBS <12584491+Maciej-GBS@users.noreply.github.com>
2021-11-11 11:56:36 +0100

    Grouped and sorted ui setup

--- fractal/MainWindow.py
+++ fractal/MainWindow.py
@@ -35,65 +35,44 @@ class MainWindowLayout(object):
         self.owner.setWindowTitle("Fractal Generator")
         self.owner.setObjectName("MainWindow")
         self.owner.resize(944, 697)
-
         self.centralwidget = QtWidgets.QWidget(self.owner)
         self.centralwidget.setObjectName("centralwidget")
-
-        self.gridLayout_2 = QtWidgets.QGridLayout(self.centralwidget)
-        self.gridLayout_2.setObjectName("gridLayout_2")
-
+        self.centralwidget.setStyleSheet("border-bottom: 1px solid black")
         self.gridLayout = QtWidgets.QGridLayout()
         self.gridLayout.setSizeConstraint(QtWidgets.QLayout.SetDefaultConstraint)
         self.gridLayout.setObjectName("gridLayout")
+        self.gridLayout.setColumnStretch(0, 1)
+        self.gridLayout.setColumnStretch(1, 6)
+        self.populateWidgets()
+        self.setTabOrder()
+        self.gridLayout_2 = QtWidgets.QGridLayout(self.centralwidget)
+        self.gridLayout_2.setObjectName("gridLayout_2")
+        self.gridLayout_2.addLayout(self.gridLayout, 0, 0, 1, 1)
+        self.owner.setCentralWidget(self.centralwidget)
+        self.addStatusBar()
 
-        self.gText = QtWidgets.QPlainTextEdit(self.centralwidget)
-        self.gText.setObjectName("gText")
-        self.gText.setPlainText("1")
-        self.gridLayout.addWidget(self.gText, 13, 0, 1, 1)
+    def addStatusBar(self):
+        self.statusbar = QtWidgets.QStatusBar(self.owner)
+        self.statusbar.setObjectName("statusbar")
+        self.statusbar.showMessage("Click generate to start...")
+        self.owner.setStatusBar(self.statusbar)
 
-        self.label_4 = QtWidgets.QLabel(self.centralwidget)
-        self.label_4.setObjectName("label_4")
-        self.label_4.setText("Constant FI:")
-        self.gridLayout.addWidget(self.label_4, 8, 0, 1, 1)
+    def addMenuBar(self):
+        self.menubar = QtWidgets.QMenuBar(self.owner)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 944, 25))
+        self.menubar.setObjectName("menubar")
+        self.owner.setMenuBar(self.menubar)
 
-        self.zoomSpin = QtWidgets.QDoubleSpinBox(self.centralwidget)
-        self.zoomSpin.setDecimals(4)
-        self.zoomSpin.setMinimum(0.0001)
-        self.zoomSpin.setMaximum(10000000000.0)
-        self.zoomSpin.setSingleStep(0.1)
-        self.zoomSpin.setValue(1.0)
-        self.zoomSpin.setObjectName("zoomSpin")
-        self.gridLayout.addWidget(self.zoomSpin, 6, 0, 1, 1)
-
-        self.colorButton = QtWidgets.QPushButton(self.centralwidget)
-        self.colorButton.setObjectName("colorButton")
-        self.colorButton.setText("Colormap...")
-        self.gridLayout.addWidget(self.colorButton, 4, 0, 1, 1)
-
+    def populateWidgets(self):
         self.savButton = QtWidgets.QPushButton(self.centralwidget)
         self.savButton.setObjectName("savButton")
         self.savButton.setText("Export as")
         self.gridLayout.addWidget(self.savButton, 0, 0, 1, 1)
 
-        self.progressBar = QtWidgets.QProgressBar(self.centralwidget)
-        self.progressBar.setValue(100)
-        self.progressBar.setObjectName("progressBar")
-        self.gridLayout.addWidget(self.progressBar, 14, 0, 1, 1)
-
         self.genButton = QtWidgets.QPushButton(self.centralwidget)
         self.genButton.setObjectName("genButton")
         self.genButton.setText("Generate")
         self.gridLayout.addWidget(self.genButton, 1, 0, 1, 1)
-
-        self.label_3 = QtWidgets.QLabel(self.centralwidget)
-        self.label_3.setObjectName("label_3")
-        self.label_3.setText("Zoom:")
-        self.gridLayout.addWidget(self.label_3, 5, 0, 1, 1)
-
-        self.graphicsView = CustomGraphics(self.centralwidget)
-        self.graphicsView.setCursor(QtCore.Qt.OpenHandCursor)
-        self.graphicsView.setObjectName("graphicsView")
-        self.gridLayout.addWidget(self.graphicsView, 0, 1, 15, 1)
 
         self.label_2 = QtWidgets.QLabel(self.centralwidget)
         self.label_2.setObjectName("label_2")
@@ -107,6 +86,35 @@ class MainWindowLayout(object):
         self.iterSpin.setValue(100)
         self.iterSpin.setObjectName("iterSpin")
         self.gridLayout.addWidget(self.iterSpin, 3, 0, 1, 1)
+
+        self.colorButton = QtWidgets.QPushButton(self.centralwidget)
+        self.colorButton.setObjectName("colorButton")
+        self.colorButton.setText("Colormap...")
+        self.gridLayout.addWidget(self.colorButton, 4, 0, 1, 1)
+
+        self.label_3 = QtWidgets.QLabel(self.centralwidget)
+        self.label_3.setObjectName("label_3")
+        self.label_3.setText("Zoom:")
+        self.gridLayout.addWidget(self.label_3, 5, 0, 1, 1)
+
+        self.zoomSpin = QtWidgets.QDoubleSpinBox(self.centralwidget)
+        self.zoomSpin.setDecimals(4)
+        self.zoomSpin.setMinimum(0.0001)
+        self.zoomSpin.setMaximum(10000000000.0)
+        self.zoomSpin.setSingleStep(0.1)
+        self.zoomSpin.setValue(1.0)
+        self.zoomSpin.setObjectName("zoomSpin")
+        self.gridLayout.addWidget(self.zoomSpin, 6, 0, 1, 1)
+
+        self.resetButton = QtWidgets.QPushButton(self.centralwidget)
+        self.resetButton.setObjectName("resetButton")
+        self.resetButton.setText("Reset")
+        self.gridLayout.addWidget(self.resetButton, 7, 0, 1, 1)
+
+        self.label_4 = QtWidgets.QLabel(self.centralwidget)
+        self.label_4.setObjectName("label_4")
+        self.label_4.setText("Constant FI:")
+        self.gridLayout.addWidget(self.label_4, 8, 0, 1, 1)
 
         self.fiSlider = QtWidgets.QSlider(self.centralwidget)
         self.fiSlider.setMaximum(1000)
@@ -132,27 +140,22 @@ class MainWindowLayout(object):
         self.fText.setPlainText("1x^2")
         self.gridLayout.addWidget(self.fText, 12, 0, 1, 1)
 
-        self.resetButton = QtWidgets.QPushButton(self.centralwidget)
-        self.resetButton.setObjectName("resetButton")
-        self.resetButton.setText("Reset")
-        self.gridLayout.addWidget(self.resetButton, 7, 0, 1, 1)
+        self.gText = QtWidgets.QPlainTextEdit(self.centralwidget)
+        self.gText.setObjectName("gText")
+        self.gText.setPlainText("1")
+        self.gridLayout.addWidget(self.gText, 13, 0, 1, 1)
 
-        self.gridLayout.setColumnStretch(0, 1)
-        self.gridLayout.setColumnStretch(1, 6)
-        self.gridLayout_2.addLayout(self.gridLayout, 0, 0, 1, 1)
+        self.progressBar = QtWidgets.QProgressBar(self.centralwidget)
+        self.progressBar.setValue(100)
+        self.progressBar.setObjectName("progressBar")
+        self.gridLayout.addWidget(self.progressBar, 14, 0, 1, 1)
 
-        self.owner.setCentralWidget(self.centralwidget)
+        self.graphicsView = CustomGraphics(self.centralwidget)
+        self.graphicsView.setCursor(QtCore.Qt.OpenHandCursor)
+        self.graphicsView.setObjectName("graphicsView")
+        self.gridLayout.addWidget(self.graphicsView, 0, 1, 15, 1)
 
-        self.menubar = QtWidgets.QMenuBar(self.owner)
-        self.menubar.setGeometry(QtCore.QRect(0, 0, 944, 25))
-        self.menubar.setObjectName("menubar")
-        self.owner.setMenuBar(self.menubar)
-
-        self.statusbar = QtWidgets.QStatusBar(self.owner)
-        self.statusbar.setObjectName("statusbar")
-        self.statusbar.showMessage("Hello world")
-        self.owner.setStatusBar(self.statusbar)
-
+    def setTabOrder(self):
         self.owner.setTabOrder(self.savButton, self.genButton)
         self.owner.setTabOrder(self.genButton, self.iterSpin)
         self.owner.setTabOrder(self.iterSpin, self.colorButton)
